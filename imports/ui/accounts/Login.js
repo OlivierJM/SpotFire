a1b2db1f9e79d43838fe23e897ba688c90219345
Olivier JM <manolivier93@gmail.com>
2018-10-20 16:17:03 +0200

    added a login logic

--- imports/ui/accounts/Login.js
+++ imports/ui/accounts/Login.js
@@ -1,27 +1,69 @@
 import React, { Component } from "react";
-import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import { Button, Row, Col, Form, FormGroup, Label, Input, FormText } from "reactstrap";
+import { Accounts } from 'meteor/accounts-base';
 
-const Login = () => (
-  <Form>
-    <FormGroup>
-      <Label for="exampleEmail">Email</Label>
-      <Input
-        type="email"
-        name="email"
-        id="exampleEmail"
-        placeholder="with a placeholder"
-      />
-    </FormGroup>
-    <FormGroup>
-      <Label for="examplePassword">Password</Label>
-      <Input
-        type="password"
-        name="password"
-        id="examplePassword"
-        placeholder="password placeholder"
-      />
-    </FormGroup>
-  </Form>
-);
+class Login extends Component{
+    state = {
+        email: '',
+        pass:''
+    }
+
+    getText = ({ target: { value }}, type) => {
+        switch (type) {
+            case 'email':
+                this.setState({
+                    email: value
+                })
+                break;
+            case 'pass':
+                this.setState({
+                    pass: value
+                })
+            default:
+                break;
+        }
+    
+    }
+    registerUser = () => {
+        const { email, pass } = this.state;
+        // login in the user
+        Meteor.loginWithPassword(email, pass, err => {
+            err ? console.log(err.reason) : console.log('successfully loggedn in the user')
+        })
+
+    }
+
+
+    render(){
+        const { email, pass } = this.state
+        return(
+            <Form>
+            <Row form>
+                <Col md={6}>
+                <Label for="exampleEmail">Email</Label>
+                    <Input
+                        type="email"
+                        value={email}
+                        placeholder="with a placeholder"
+                        onChange={e => this.getText(e, 'email')}
+                    />
+                </Col>
+                <Col md={6}>
+                <Label for="exampleEmail">Password</Label>
+                    <Input
+                        type="email"
+                        value={pass}
+                        placeholder="with a placeholder"
+                        onChange={e => this.getText(e, 'pass')}
+                    />
+                </Col>
+                <Button 
+                    onClick={this.loginUser}
+                    color="primary">Login</Button>
+            </Row>
+          </Form>
+        )
+    }
+}
 
 export default Login;
