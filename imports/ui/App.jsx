3b213b9006f28cb896825b8e74156e201e4ea3f7
Olivier JM <manolivier93@gmail.com>
2018-10-20 20:17:29 +0200

    getting user's location

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,11 +2,12 @@ import React, { Fragment } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom'
 import Login from './accounts/Login'
 import Register from './accounts/Register'
+import CreatePost from './posts/Posts'
 
 const App = () => (
   <BrowserRouter>
     <Fragment>
-      <Route path='/' render={ () => <Link to='login'>Hello I am Home</Link>} />
+      <Route path='/' component={CreatePost} />
       <Route path="/login" component={Login} />
       <Route path="/register" component={Register} />
     </Fragment>
